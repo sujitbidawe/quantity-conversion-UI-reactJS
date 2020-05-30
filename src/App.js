cf4cb934a83a7c102dd6c2a5f63883b355f6d007
sujitbidawe <bidawesujit@gmail.com>
2020-05-30 12:24:24 +0530

    added: code to display Quantity and units

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Quantity from './Quantity';
 
-function App() {
+
+class App extends Component {
+  
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div>
+        <h2>Quantity Convertor</h2>
+      </div>
+      <br/>
+      <div>
+        <Quantity/>
+      </div>
     </div>
   );
+ }
 }
 
 export default App;
